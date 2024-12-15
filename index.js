d778e5d3baa09259ae995ef8a05b0d05ef8a2e13
jfmtucci <jfmtucci@gmail.com>
2024-12-15 13:04:38 -0300

    Subiendo 2do Desafio Proyecto Likeme

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express(),
   pool = new Pool({
     host: "localhost",
     user: "postgres",
-    password: "Pnuevo987",
+    password: "",
     database: "likeme",
     allowExitOnIdle: true,
   });
@@ -22,31 +22,63 @@ async function getPosts() {
 }
 
 app.get("/posts", async (req, res) => {
-  const posts = await getPosts();
-  res.json(posts);
+  try {
+    const posts = await getPosts();
+    res.json(posts);
+  } catch (error) {
+    res.json({
+      message: err.message,
+      err,
+    });
+  }
 });
 
 app.post("/posts", async (req, res) => {
-  const like = 0;
-  const { titulo, url, descripcion } = req.body;
-  const consulta = `insert into posts(titulo, img, descripcion,likes)values($1,$2,$3,$4)`;
-  const values = [titulo, url, descripcion, like];
-  //console.log(values);
+  try {
+    const like = 0;
+    const { titulo, url, descripcion } = req.body;
+    if (!titulo || !url || !descripcion) {
+      return res.status(400).json({
+        message:
+          "El Titulo, URL y Descripcion son requeridos y no pueden ser nulos o indefinidos.",
+      });
+    }
+    const consulta = `insert into posts(titulo, img, descripcion,likes)values($1,$2,$3,$4)`;
+    const values = [titulo, url, descripcion, like];
+    //console.log(values);
 
-  await pool.query(consulta, values);
-  res.status(200).send("Nuevo Post Añadido");
+    await pool.query(consulta, values);
+    res.status(200).send("Nuevo Post Añadido");
+  } catch (err) {
+    const { code } = err;
+    if (code == "23502") {
+      res
+        .status(400)
+        .send(
+          "Se ha violado la restricción NOT NULL en uno de los campos de la tabla"
+        );
+    } else {
+      res.status(500).json({
+        message: err.message,
+        err,
+      });
+    }
+  }
 });
 
 app.put("/posts/like/:id", async (req, res) => {
-  const { id } = req.params;
-  //const value = [id];
-  //const { likes } = req.body;
   try {
-    const query =
-      "UPDATE posts SET likes = likes + 1 WHERE id = $1 RETURNING *;";
+    const { id } = req.params;
     const values = [id];
-    //    console.log(query);
-    const result = await pool.query(query, values);
+    if (!id) {
+      throw new Error(
+        "El ID es requeridos y no pueden ser nulos o indefinidos."
+      );
+    }
+    const result = await pool.query(
+      "UPDATE posts SET likes = likes + 1 WHERE id = $1 RETURNING *;",
+      values
+    );
 
     if (result.rowCount === 0) {
       return res.status(404).send("Error");
@@ -56,21 +88,22 @@ app.put("/posts/like/:id", async (req, res) => {
       message: "likes modificada correctamente",
       post: result.rows[0],
     });
-  } catch (error) {
-    console.error(error);
+  } catch ({ code, message }) {
+    //console.error(err);
+    res.status(code).send(message);
     res.status(500).send("Error al modificar Post");
+    //res.json({ message: err.message, err });
   }
 });
 
 app.delete("/posts/:id", async (req, res) => {
-  const { id } = req.params;
-  const values = [id];
-
   try {
-    const query = "delete from posts WHERE id = $1  RETURNING *";
-
-    //console.log(query);
-    const result = await pool.query(query, values);
+    const { id } = req.params;
+    const values = [id];
+    const result = await pool.query(
+      "delete from posts WHERE id = $1  RETURNING *",
+      values
+    );
 
     if (result.rowCount === 0) {
       return res.status(404).send("Error");
@@ -79,9 +112,10 @@ app.delete("/posts/:id", async (req, res) => {
     res.send({
       message: "Post Eliminado correctamente",
     });
-  } catch (error) {
-    console.error(error);
+  } catch (err) {
+    //console.error(error);
     res.status(500).send("Error al eliminar Post");
+    res.json({ msg: err.message, err });
   }
 });
 
